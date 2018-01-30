2ce44cfe19dd2c3f71fa6138702337c294cc6b94
ShuffleTanker <zhirong.mark.huang@gmail.com>
2018-01-29 21:48:59 -0500

    Init

---
+++ TradeSatoshiAPI/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeSatoshiAPI
+{
+    public class Class1
+    {
+    }
+}
